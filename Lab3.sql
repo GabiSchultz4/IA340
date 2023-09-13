6c71dc8144c92d7bef3fa181d84f12a5bf4d4c20
GabiSchultz4 <122939253+GabiSchultz4@users.noreply.github.com>
2023-09-13 17:54:49 -0400

    Update Lab3.sql

--- Lab3.sql
+++ Lab3.sql
@@ -25,3 +25,10 @@ values
 ('s4@jmu.edu','c2'),
 ('s2@jmu.edu','c3'),
 ('s3@jmu.edu','c3')
+
+--Q2.5
+insert into professor (p_email,p_name,"office ")
+values ('p3@jmu.edu','p3','o3');
+
+insert into course (c_number,c_name,room,p_email)
+values ('c4','facebook','r1','p3@jmu.edu')
